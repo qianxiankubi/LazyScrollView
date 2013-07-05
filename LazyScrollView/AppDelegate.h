03e1ea4e4cf3117b416b92a8fd5aead248c1f63f
zzltjnh <zzltjnh@gmail.com>
2013-07-05 16:17:04 +0800

    14
    push

---
+++ LazyScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LazyScrollView
+//
+//  Created by Zhilong Zhang on 5/6/13.
+//  Copyright (c) 2013 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LazyScrollView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LazyScrollView
+//
+//  Created by Zhilong Zhang on 5/6/13.
+//  Copyright (c) 2013 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
